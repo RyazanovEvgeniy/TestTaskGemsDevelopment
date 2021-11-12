86fb86fd515e4c21f66716c51d321a71f21ffdae
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2021-11-12 15:48:31 +0600

    Add xUnitTest to QuadraticEquation project

---
+++ QuadraticEquation/QuadraticEquationTest/CalcTests.cs
@@ -0,0 +1,63 @@
+using System;
+using Xunit;
+using QuadraticEquation;
+
+namespace QuadraticEquationTests
+{
+    public class CalcTests
+    {
+        [Fact]
+        public void SolveQuadraticEquationTest1()
+        {
+            // Arrange
+            double a = 1.0;
+            double b = 0.0;
+            double c = 1.0;
+
+            double[] expected = null;
+
+            // Act
+            Calc calc = new Calc(); ;
+            double[] actual = calc.SolveQuadraticEquation(a, b, c);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SolveQuadraticEquationTest2()
+        {
+            // Arrange
+            double a = 1.0;
+            double b = 2.0;
+            double c = 1.0;
+
+            double[] expected = { -1, -1 };
+
+            // Act
+            Calc calc = new Calc(); ;
+            double[] actual = calc.SolveQuadraticEquation(a, b, c);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SolveQuadraticEquationTest3()
+        {
+            // Arrange
+            double a = 1.0;
+            double b = 5.0;
+            double c = 4.0;
+
+            double[] expected = { -1.0, -4.0 };
+
+            // Act
+            Calc calc = new Calc(); ;
+            double[] actual = calc.SolveQuadraticEquation(a, b, c);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
