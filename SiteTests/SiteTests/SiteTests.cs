2bed84a2de55ee9f7a3413611e22f01842cef0c6
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2021-11-12 17:00:30 +0600

    Add SiteTests project

---
+++ SiteTests/SiteTests/SiteTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SiteTests
+{
+    public class SiteTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
