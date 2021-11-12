69bb40aad4e32130178743a10b34fb5ba203b9a7
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2021-11-12 19:29:04 +0600

    SiteTests. Smth works.

--- SiteTests/SiteTests/SiteTests.cs
+++ SiteTests/SiteTests/SiteTests.cs
@@ -1,18 +1,73 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace SiteTests
 {
-    public class SiteTests
+    public class GemsDevSiteTests
     {
+        private IWebDriver driver;
+
         [SetUp]
         public void Setup()
         {
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://gemsdev.ru");
+            driver.FindElement(By.LinkText("Продукты")).Click();
         }
 
         [Test]
-        public void Test1()
+        public void FindGeoMeta()
         {
-            Assert.Pass();
+            driver.FindElement(By.CssSelector("section[text*='GeoMeta']"));
+        }
+
+        [Test]
+        public void FindGosSystem()
+        {
+            driver.FindElement(By.CssSelector("section[class*=gos-system]"));
+        }
+
+        [Test]
+        public void FindGosSystemLink()
+        {
+            var elements = driver.FindElements(By.XPath("//a[text()='GeoMeta']"));
+
+            TestContext.WriteLine("!!!");
+
+            foreach (var element in elements)
+                TestContext.WriteLine("tag " + element.TagName +
+                    " Text " + element.Text +
+                    " href " + element.GetAttribute("href") +
+                    " name " + element.GetAttribute("name") +
+                    " text " + element.GetAttribute("text"));
+            //var elements = driver.FindElements(By.CssSelector("#menu-item-45 > a"));
+            //foreach (var element in elements)
+            //    TestContext.WriteLine(element.TagName + " " +
+            //        element.Text + " " +
+            //        element.GetAttribute("href"));
+        }
+
+        [Test]
+        public void FindUrbanAnalytics()
+        {
+            driver.FindElement(By.CssSelector("section[class*=urban-analytics]"));
+        }
+
+        [Test]
+        public void FindOtherProducts()
+        {
+            driver.FindElement(By.CssSelector("section[class*=other-products]"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
         }
     }
 }
